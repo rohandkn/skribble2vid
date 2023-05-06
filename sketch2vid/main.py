78792429dc37cdbbcfc122e1aadb717c1cf6894e
Vignesh Rajmohan <vrajmoha@andrew.cmu.edu>
2023-05-05 21:10:52 -0400

    Update main.py

--- sketch2vid/main.py
+++ sketch2vid/main.py
@@ -10,6 +10,11 @@ number_of_sketch_frames = 3 #@param {type:"slider", min:1, max:7, step:1}
 def generate_video(prompt, *sketches):
     input_sketches_path = 'rife-interop/input_images'
     os.makedirs(input_sketches_path, exist_ok=True)
+    
+    
+    # print("hellooooooooooooooo")
+    # print(prompt)
+    # print(sketches)
 
     # Save sketches as PNGs
     for idx, sketch in enumerate(sketches):
@@ -21,7 +26,7 @@ def generate_video(prompt, *sketches):
     subprocess.run(['python3', 'inference_video.py', '--img', 'input_images', '--png'], cwd='rife-interop')
 
     # Run the run_tests.py script
-    subprocess.run(['python3', 'run_tests.py'], cwd='vid2vid')
+    subprocess.run(['python3', 'run_tests.py', '--prompt', prompt], cwd='vid2vid')
 
     # Load the output video
     output_video_path = 'vid2vid/output_vid/output.mp4'
